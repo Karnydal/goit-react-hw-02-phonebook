6bcbb6c3508b4cfb5fa33f86ee411d882294121f
Karnydal <97188586+Karnydal@users.noreply.github.com>
2022-08-12 19:26:02 +0300

    add

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types';
+import { List, ContactCard, Name, Number, DeleteBtn } from './ContactList.styled';
+
+export const ContactList = ({ contacts, onDeleleButton }) => {
+    return (
+        <List>
+            {contacts.map(({ name, id, number }) => {
+                return (
+                    <ContactCard key={id}>
+                        <Name>{name}</Name>
+                        <Number>{number}</Number>
+                        <DeleteBtn type="button" id={id} onClick={onDeleleButton}>
+                            Delete
+                        </DeleteBtn>
+                    </ContactCard>
+                )
+            })}
+        </List>
+    )
+};
+
+ContactList.prototypes = {
+    contacts: PropTypes.arrayOf(
+        PropTypes.exact({
+            id: PropTypes.string.isRequired,
+            name: PropTypes.string.isRequired,
+            number: PropTypes.string.isRequired,
+        })
+    ),
+    onDeleleButton: PropTypes.func.isRequired,
+};
